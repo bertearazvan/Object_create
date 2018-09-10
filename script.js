37137b97acc0098f5221c2f1f8987806ecbb2108
bertea74@gmail.com <bertea74@gmail.com>
2018-09-10 12:34:31 +0200

    object create

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
 "use strict";
 
-const names = ["Harry Potter",
-            "Ron Weasley",
-            "Hermione Granger",
-            "Neville Longbottom",
-            "Luna Lovegood",
-            "Cho Chang",
-            "Justin Finch-Fletchly",
-            "Draco Malfoy"];
+const names = [
+  "Harry Potter",
+  "Ron Weasley",
+  "Hermione Granger",
+  "Neville Longbottom",
+  "Luna Lovegood",
+  "Cho Chang",
+  "Justin Finch-Fletchly",
+  "Draco Malfoy"
+];
+
+const Student = {
+  firstName: "",
+  lastName: "",
+  toString() {
+    return this.firstName + " " + this.lastName;
+  },
+  splitNames(fullName) {
+    const firstSpace = fullName.indexOf(" ");
+    this.firstName = fullName.substring(0, firstSpace);
+    this.lastName = fullName.substring(firstSpace + 1);
+  }
+};
 
 const houses = {
-    Gryffindor: "Gryffindor",
-    Hufflepuff: "Hufflepuff",
-    Ravenclaw: "Ravenclaw",
-    Slytherin: "Slytherin"
-}
+  Gryffindor: "Gryffindor",
+  Hufflepuff: "Hufflepuff",
+  Ravenclaw: "Ravenclaw",
+  Slytherin: "Slytherin"
+};
 
 const students = [];
 
 function createStudents() {
-    // ... TODO: build this function ...
+  names.forEach(function(student) {
+    console.log(student);
+    const newName = Object.create(Student);
+    newName.splitNames(student);
+
+    const randomHouse = Object.keys(houses)[
+      Math.floor(Math.random()) * Object.keys(houses).length
+    ];
+    newName.house = randomHouse;
+    students.push(newName);
+  });
 }
+
+createStudents();
+
+console.table(students);
